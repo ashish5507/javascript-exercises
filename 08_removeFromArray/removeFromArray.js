d5845d0f1743a95ce27e55d21995d40d99e4a042
Ashish Joshi <ashishjoshirj57@gmail.com>
2025-06-06 11:47:00 +0530

    create function and remove skip from tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    if(arguments[1] === undefined){
+        return arr;
+    }
+
+    let args = Array.prototype.slice.call(arguments,1);
+    let size = args.length;
+    for(let i = 0; i<size; i++){
+        let ele = args[i];
+
+        while(true){
+            let index = arr.indexOf(ele);
+            if(index == -1){
+                break;
+            }
+            arr.splice(index,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
