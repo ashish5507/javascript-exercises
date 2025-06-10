da96e6d4cc485c1f4f90fc96f280cee23f00dc74
Ashish Joshi <ashishjoshirj57@gmail.com>
2025-06-10 16:41:23 +0530

    write code for the functions specified

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let ans = 0;
+  for(let a of arr){
+    ans+=a;
+  }
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let ans = 1;
+  for(let i of arr){
+    ans = ans*i;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ans = 1;
+  for(let i = a; i>0; i--){
+    ans = ans*i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
